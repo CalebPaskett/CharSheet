2103dc9b9c242835672ef5b27d741b6f01c7caba
Caleb Paskett <a02168686@aggies.usu.edu>
2022-05-21 12:26:49 -0600

    Implemented basic modal for about

--- src/components/tabs/about.js
+++ src/components/tabs/about.js
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react';
 import {doc, setDoc, getFirestore} from "firebase/firestore";
-import { AboutModal } from '../modals/about_modal';
 
 export const About = (props) => {
   const [loading, setLoading] = useState(true);
@@ -8,11 +7,13 @@ export const About = (props) => {
   const [modal, setModal] = useState(false);
 
   const [about, setAbout] = useState(null);
+  const [tempAbout, setTempAbout] = useState(null);
 
   useEffect(() => {
     document.title = (props.character.about.name + " / About - Hero Sheet");
 
     setAbout(props.character.about);
+    setTempAbout(props.character.about);
 
 		setLoading(false);
   }, [props.character]);
@@ -22,10 +23,13 @@ export const About = (props) => {
 
     const db = getFirestore();
     await setDoc(doc(db, ("users/"+props.user.uid+"/characters"), props.character.id), {
-			about: about,
+			about: tempAbout,
     }, {merge: true})
 
-    document.title = (about.name + " / About - Hero Sheet");
+    setAbout(tempAbout);
+
+    document.title = (tempAbout.name + " / About - Hero Sheet");
+    setModal(false);
     setSaveStat("Saved!");
     setTimeout(function() {setSaveStat("Save Changes");}, 500);
   }
@@ -36,35 +40,49 @@ export const About = (props) => {
 
   return (
       <div>
-        {modal && <AboutModal about={about}/>}
+        {modal && <div className="modal-container">
+          <div className="overlay" onClick={() => (setModal(false))}/>
+          <div className="modal" onClick={(e) => e.stopPropagation()}>
+            <h3>Edit About</h3>
 
-        <div>Name:</div>
-        <input type="text" value={about.name} onChange={(e) => setAbout(oldAbout => ({...oldAbout, name: e.target.value}))}/>
+            <div>Name:</div>
+            <input type="text" value={tempAbout.name} onChange={(e) => setTempAbout(oldAbout => ({...oldAbout, name: e.target.value}))}/>
 
-        <div>Other Names:</div>
-        <input type="text" value={about.nicknames} onChange={(e) => setAbout(oldAbout => ({...oldAbout, nicknames: e.target.value}))}/>
+            <div>Other Names:</div>
+            <input type="text" value={tempAbout.nicknames} onChange={(e) => setTempAbout(oldAbout => ({...oldAbout, nicknames: e.target.value}))}/>
 
-        <div>Background:</div>
-        <textarea value={about.background} onChange={(e) => setAbout(oldAbout => ({...oldAbout, background: e.target.value}))}/>
+            <div>Background:</div>
+            <textarea value={tempAbout.background} onChange={(e) => setTempAbout(oldAbout => ({...oldAbout, background: e.target.value}))}/>
 
-        <div>Personality:</div>
-        <textarea value={about.personality} onChange={(e) => setAbout(oldAbout => ({...oldAbout, personality: e.target.value}))}/>
+            <div>Personality:</div>
+            <textarea value={tempAbout.personality} onChange={(e) => setTempAbout(oldAbout => ({...oldAbout, personality: e.target.value}))}/>
 
-        <div>Quote:</div>
-        <textarea value={about.quote} onChange={(e) => setAbout(oldAbout => ({...oldAbout, quote: e.target.value}))}/>
+            <div>Quote:</div>
+            <textarea value={tempAbout.quote} onChange={(e) => setTempAbout(oldAbout => ({...oldAbout, quote: e.target.value}))}/>
 
-        <div>Tactics:</div>
-        <textarea value={about.tactics} onChange={(e) => setAbout(oldAbout => ({...oldAbout, tactics: e.target.value}))}/>
+            <div>Tactics:</div>
+            <textarea value={tempAbout.tactics} onChange={(e) => setTempAbout(oldAbout => ({...oldAbout, tactics: e.target.value}))}/>
 
-        <div>Useage:</div>
-        <textarea value={about.useage} onChange={(e) => setAbout(oldAbout => ({...oldAbout, useage: e.target.value}))}/>
+            <div>Useage:</div>
+            <textarea value={tempAbout.useage} onChange={(e) => setTempAbout(oldAbout => ({...oldAbout, useage: e.target.value}))}/>
 
-        <div>Appearance:</div>
-        <textarea value={about.appearance} onChange={(e) => setAbout(oldAbout => ({...oldAbout, appearance: e.target.value}))}/>
+            <div>Appearance:</div>
+            <textarea value={tempAbout.appearance} onChange={(e) => setTempAbout(oldAbout => ({...oldAbout, appearance: e.target.value}))}/>
 
-        <button type="button" className="button" onClick={() => (setModal(true))}>Modal</button>
+            <button type="button" className="button" onClick={saveChanges}>Save</button>
+          </div>
+        </div>}
 
-        <button type="button" className="save button" onClick={saveChanges}>{saveStat}</button>
+        <div>Name: {about.name}</div>
+        <div>Other Names: {about.nicknames}</div>
+        <div>Background: {about.background}</div>
+        <div>Personality: {about.personality}</div>
+        <div>Quote: {about.quote}</div>
+        <div>Tactics: {about.tactics}</div>
+        <div>Useage: {about.useage}</div>
+        <div>Appearance: {about.appearance}</div>
+
+        {!modal && <button type="button" className="save button" onClick={() => (setModal(true))}>Make Changes</button>}
       </div>
   );
 }
